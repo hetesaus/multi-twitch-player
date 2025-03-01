5efee7e813313ff7a7771c1dd784a4d2a1d55a18
hetesaus <mhs@live.nl>
2025-03-01 20:50:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ function getQueryParam(param) {
 	return urlParams.get(param);
 }
 
-const channel1Name = getQueryParam("channel1");
-const channel2Name = getQueryParam("channel2");
+const channel1Name = getQueryParam("channel").split(",")[0];
+const channel2Name = getQueryParam("channel2").split(",")[1];
 
-if (channelName) {
+if (channel1Name) {
 	const player1 = document.getElementById("player1");
-	const player2 = document.getElementById("player2");
-	
 	player1.src = `https://player.twitch.tv/?channel=${channel1Name}&parent=${domain}`;
-	player1.src = `https://player.twitch.tv/?channel=${channel2Name}&parent=${domain}`;
-	
-	document.title = "multi-twitch";
 }
+
+if (channel2Name) {
+	const player2 = document.getElementById("player2");
+	player2.src = `https://player.twitch.tv/?channel=${channel2Name}&parent=${domain}`;
+}
+
+document.title = "multi-twitch";
