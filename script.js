fa9e55e1a0c0f3d19aa18b66542847836cab5bd6
hetesaus <mhs@live.nl>
2025-03-01 20:56:20 +0100

    Test embed

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 const domain = "hetesaus.github.io";
 
 function getQueryParam(param) {
-	const urlParams = new URLSearchParams(window.location.search);
-	return urlParams.get(param);
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
 }
 
+// Get channels from URL parameters
 const channel1Name = getQueryParam("channel1");
 const channel2Name = getQueryParam("channel2");
 
-if (channel1Name) {
-	const player1 = document.getElementById("player1");
-	player1.src = `https://player.twitch.tv/?channel=${channel1Name}&parent=${domain}`;
+// Load the Twitch Player API script dynamically
+const script = document.createElement("script");
+script.src = "https://embed.twitch.tv/embed/v1.js";
+script.onload = initializePlayers;
+document.body.appendChild(script);
+
+// Global player variables
+let player1, player2;
+
+function initializePlayers() {
+    if (channel1Name) {
+        player1 = new Twitch.Player("player1", {
+            channel: channel1Name,
+            width: "100%",
+            height: "100%",
+            parent: [domain]
+        });
+
+        player1.addEventListener(Twitch.Player.READY, () => {
+            player1.setMuted(false); // Player 1 starts unmuted
+        });
+    }
+
+    if (channel2Name) {
+        player2 = new Twitch.Player("player2", {
+            channel: channel2Name,
+            width: "100%",
+            height: "100%",
+            parent: [domain]
+        });
+
+        player2.addEventListener(Twitch.Player.READY, () => {
+            player2.setMuted(true); // Player 2 starts muted
+        });
+    }
 }
 
-if (channel2Name) {
-	const player2 = document.getElementById("player2");
-	player2.src = `https://player.twitch.tv/?channel=${channel2Name}&parent=${domain}`;
+// Function to switch mute states
+function switchMute() {
+    if (player1 && player2) {
+        const isPlayer1Muted = player1.isMuted();
+        player1.setMuted(!isPlayer1Muted);
+        player2.setMuted(isPlayer1Muted);
+    }
 }
-
-document.title = "multi-twitch";
